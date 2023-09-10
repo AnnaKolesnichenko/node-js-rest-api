76793462d7e9aa51bf8870fb52cc7fb1a053e0ce
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-09-10 15:40:24 +0300

    new

--- models/Contact.js
+++ models/Contact.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { handleSaveError } from "./hooks";
 
 const contactSchema = new Schema({
     name: {
@@ -17,7 +18,9 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     }
-});
+}, {versionKey: false, timestamps: true});
+
+contactSchema.post('save', handleSaveError)
 
 const Contact = model('contact', contactSchema);
 
